206cd38a01f060173268eb00563b4bc561938f4b
mayank-sengar <btech10111.23@bitmesra.ac.in>
2025-07-27 02:36:37 +0530

    fix dirname

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,13 @@ import connectDB from './config/db.js';
 import chatRoutes from './routes/chat.route.js'
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 
+// ES module compatible __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 // this is necessary for multer to store temporary files before uploading to Cloudinary
 const tempDir = path.join(__dirname, 'public', 'temp');
 if (!fs.existsSync(tempDir)) {
