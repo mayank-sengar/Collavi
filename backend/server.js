81a5608d4afb817dd00c752f93aa11d3ed32fb5e
mayank-sengar <btech10111.23@bitmesra.ac.in>
2025-07-27 02:33:55 +0530

    add dir for multer

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,19 @@ import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js'
 import connectDB from './config/db.js';
 import chatRoutes from './routes/chat.route.js'
+import fs from 'fs';
+import path from 'path';
 
 const app = express();
+
+// this is necessary for multer to store temporary files before uploading to Cloudinary
+const tempDir = path.join(__dirname, 'public', 'temp');
+if (!fs.existsSync(tempDir)) {
+  fs.mkdirSync(tempDir, { recursive: true });
+}
 app.use(cors({
     origin: `${process.env.FRONTEND_URL || "http://localhost:5173"}`, 
-    credentials: true, //allow frontend to send requests 
+    credentials: true, //frontend to send requests 
 }));
 
 
