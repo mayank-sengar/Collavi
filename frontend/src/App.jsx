4b7dd64a9ae983c49a5d211a21328dc303d0517b
mayank-sengar <btech10111.23@bitmesra.ac.in>
2025-08-17 16:14:04 +0530

    Edit:complete chat feature with sockets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ChatPage from './pages/ChatPage';
+import ChatPage from './pages/OldChatPage.jsx';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import Notifications  from './pages/Notifications';
