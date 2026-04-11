f6f184b797ce81c08bd671261fcd21de4232b129
mayank-sengar <btech10111.23@bitmesra.ac.in>
2026-04-11 12:43:52 +0530

    redis timeout and reconnect strategy added

--- backend/utils/redisClient.js
+++ backend/utils/redisClient.js
@@ -1,22 +1,41 @@
 import { createClient } from 'redis';
 import 'dotenv/config';
 
-export default async function exeRedisClient(){
-//run this from backend : node utils/redisClient.js
-const client = createClient({
+let client; 
+
+export default async function exeRedisClient() {
+  if (client) return client; // reusing existing client
+
+  client = createClient({
     username: process.env.REDIS_USERNAME,
     password: process.env.REDIS_PASSWORD,
     socket: {
-        host: process.env.REDIS_HOST,
-        port: Number(process.env.REDIS_PORT)
+      host: process.env.REDIS_HOST,
+      port: Number(process.env.REDIS_PORT),
+
+      connectTimeout: 10000, //  avoid timeout crashes
+
+      reconnectStrategy: (retries) => {
+        console.log("Retrying Redis connection:", retries);
+        return Math.min(retries * 100, 3000); // exponential backoff,capping delay at 3 second
+      }
     }
-});
+  });
 
-client.on('error', err => console.log('Redis Client Error', err));
+  client.on('error', (err) => {
+    console.log('Redis Client Error', err);
+  });
 
-await client.connect();
-console.log("Redis server running")
+  client.on("end", () => {
+    console.log("Redis connection closed");
+  });
 
-return client;
+  client.on("reconnecting", () => {
+    console.log("Redis reconnecting...");
+  });
+
+  await client.connect();
+  console.log("Redis server running");
+
+  return client;
 }
-
