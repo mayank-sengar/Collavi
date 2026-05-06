aaddc7c1e7afccdfd4bca17cb23d969297432472
mayank-sengar <btech10111.23@bitmesra.ac.in>
2026-05-06 16:13:41 +0530

    fix: typo fix

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -59,7 +59,7 @@ const getRecommendedUsers = asyncHandler(async (req, res) => {
             $ne: currentUserId,
             $nin: currentUser.friend
         },
-        isOnboarded:trueq
+        isOnboarded:true
     }).select("fullName avatar location skills bio embeddings");
     
 
